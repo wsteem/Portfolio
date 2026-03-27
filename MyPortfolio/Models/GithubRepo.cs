41c319ec408a851e5401e8163b8e9f9b8ce89e13
Wesley Steem <102566225+wsteem@users.noreply.github.com>
2026-03-27 08:09:50 -0700

    Add files via upload

---
+++ MyPortfolio/Models/GithubRepo.cs
@@ -0,0 +1,9 @@
+namespace MyPortfolio.Models;
+
+public class GithubRepo
+{
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Html_Url { get; set; } = "";
+    public string Language { get; set; } = "";
+}
